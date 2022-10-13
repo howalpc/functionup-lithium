449cc07e18de7c8eb168f3bbd0c4ed6f1f73c9ab
howalpc <howalpc@gmail.com>
2022-10-13 20:20:00 +0530

    cal age

--- src/index.js
+++ src/index.js
@@ -13,3 +13,7 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+cal age = birthyear => 2022 - year; {
+
+}
+console.log(age(2000))
