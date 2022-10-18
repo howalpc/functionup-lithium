40786925c6fc6fabb38357278acf49e0640c9940
howalpc <howalpc@gmail.com>
2022-10-18 12:03:53 +0530

    params

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,36 +8,61 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+router.get('/movies', function (req, res){
+    ////console.log("The path params in the request are : ", req.query)
+    let movies = ['Rang de basanti', 'The shining', 'Lord of rings','Batman begins']
+    res.send(movies)
 })
 
 
 // Example 1 for path params
-router.get('/students/:studentName', function(req, res){
-    // ':' denotes that the following part of route is a variable
-    // The value of this variable is what we are sending in the request url after /students
-    // This value is set in the form of an object inside req.params
-    // The object contain key value pairs
-    // key is the variable in the route
-    // value is whatever dynamic value sent in the request url
-    let myParams = req.params
+router.get('/movies/:index', function (req, res) {
+    //console.log("movies", res.params)
+    const index = req.params.index;
+    if (index > (movies.length -1)) {
+res.send("sure")
+    } else {
+    res.send(movies[index])
+    }
+    const movies = [
+        "Rang de basanti",
+        "The shining",
+        "Lord of rings",
+        "Batman begins",
+    ];
 
-    // params attribute is fixed in a request object
-    // params contains the path parameters object
-    console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
+    res.send(movies[index])
+
 })
 
 // Example 2 for path params
-router.get('/student-details/:name', function(req, res){
-    let requestParams = req.params
-    console.log("This is the request ", requestParams)
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
+router.get('/movies_list', function (req, res) {
+    const movies_arr = [{
+        id: 1,
+        name: 'The Shining',
+    }, {
+        id: 2,
+        name: 'Incendies'
+    }, {
+        id: 3,
+        name: 'Rang de Basanti'
+    }, {
+        id: 4,
+        name: 'Finding Nemo'
+    }]
+    res.send(movies_arr)
+
+    router.get('/', function (req, res) {
+        movires1 = []
+
+        const movie = movies1.find(film => film.id == id)
+        if (movie) {
+            req.send()
+        } else {
+            req.send("no")
+        }
+    }
 })
 
 module.exports = router;
+// 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message
